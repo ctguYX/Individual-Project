fd6cf41d746804cb07a8cf0014286f3480f34031
1539575273 <1539575273@qq.com>
2019-09-26 14:30:39 +0800

    将第零步的结果换位输出到文件中，然后完成了第一步的功能1：输出文件中所有不重复的单词，按照出现次数由多到少排序

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,6 +8,10 @@
 #include<cstdio>
 #include<algorithm>
 #include<cmath>
+#include<map>
+#include<vector>
+#include<fstream>
+#include <iomanip>
 using namespace std;
 typedef long long ll;
 
@@ -55,6 +59,7 @@ void solve_c(string file)
 			}
 		}
 	}
+	fclose(book);
 
 	for (int i = 0; i < 52; i++) {
 		sum[i].pinlv = sum[i].num*1.0 / sum_alphabet;
@@ -63,27 +68,122 @@ void solve_c(string file)
 	}
 
 	sort(sum,sum + 52, cmp_solve_c);
-	cout << file << "文本文件中 26 字母出现的频率如下" << endl;
+
+
+	cout << "结果请查看debug文件夹的文件：第零步功能.txt" << endl;
+
+	freopen("第零步功能.txt", "w", stdout);
+	cout << "单词	" << "频率" << endl;
+
 	for (int i = 0; i < 52; i++) {
-		cout << sum[i].c << "的频率为";
+		cout << sum[i].c << "	";
 		printf("%.2lf\n", sum[i].pinlv);
 	}
+	fclose(stdout);
+
 
 }
 // solve -c
 /*****************************/
 
 
+/*****************************/
+// function_first_step
+struct solve_f_node {
+	string word;
+	ll sum;
+}temp_word;
+vector<solve_f_node> word;
+map<string, ll>index_word;
+map<string, ll>::iterator iterfind;
+ll sum_word=0;
+int max_size=0;
+bool cmp_solve_f(solve_f_node x, solve_f_node y) {
+	if (x.sum == y.sum)
+		return x.word < y.word;
+	else
+		return x.sum > y.sum;
+}
+
+void function_first_step(string file)
+{
+	temp_word.sum = 1;
+	ifstream book;
+	book.open(file);
+	string str;
+	while (!book.eof()) {
+		getline(book, str);
+		int size = str.size();
+		string test_word;
+		for (int i = 0; i < size; i++) {
+			if (str[i] >= 'A'&&str[i] <= 'Z') //大写字母转小写
+				str[i] = str[i] + 'a' - 'A';
+
+			if (str[i] >= 'a'&&str[i] <= 'z' || str[i] >= '0'&&str[i] <= '9') //当前位是字母或数字加到单词字符串后面
+				test_word += str[i];
+
+			else if (!test_word.empty() || (i == size - 1 && str[i] >= 'a'&&str[i] <= 'z' || str[i] >= '0'&&str[i] <= '9')) {
+
+				iterfind = index_word.find(test_word);
+				if (iterfind == index_word.end()) { //还没出现过这个单词，插入到vector中
+					temp_word.word = test_word;
+					word.push_back(temp_word);
+
+					index_word[test_word] = sum_word;//单词插入到map中
+					sum_word++;
+
+					max_size = max(max_size, int(test_word.size()));
+				}
+				else { //有这个单词，数量+1
+					word[iterfind->second].sum++;
+				}
+				test_word.clear();
+			}
+		}
+	}
+	book.close();
+	
+}
+// function_first_step
+/*****************************/
+
+/*****************************/
+// solve_f
+void printf_solve_f()
+{
+	freopen("第一步功能1.txt", "w", stdout);
+	cout <<setw(max_size+10) << setiosflags(ios::left) << "单词" << "数量" << endl;
+	for (int i = 0; i < sum_word; i++) {
+		cout << setw(max_size+10-word[i].word.size()) << setiosflags(ios::left)<< word[i].word << word[i].sum << endl;
+	}
+	fclose(stdout);
+
+}
+
+void solve_f(string file)
+{
+	function_first_step(file);
+	sort(word.begin(), word.end(), cmp_solve_f);
+	cout << "结果请查看debug文件夹的文件：第一步功能1.txt" << endl;
+	printf_solve_f();
+}
+
+// solve_f
+/*****************************/
+
+
 int main(int argc, char* argv[])
 {
-	//for (int i = 0; i<argc; i++)
-	//	cout << argv[i] << endl;
+
 
 	if (strcmp(argv[1], "-c") == 0) {
 		string file = argv[2];
 		solve_c(file);
 	}
-
+	else if (strcmp(argv[1], "-f") == 0) {
+		string file = argv[2];
+		solve_f(file);
+	}
 
     return 0;
 }
